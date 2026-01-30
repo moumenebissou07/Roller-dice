47348d485f406aa55063d2c9f26cfb5a614f3329
moumene bissou <moumenejjjj@gmail.com>
2026-01-30 14:43:56 +0100

    Fix image source path for dice images

--- dice.js
+++ dice.js
@@ -8,7 +8,7 @@ const images = [];
 
 for(let i = 0 ; i<input ;i++){
 values.push(Math.round(Math.random()*5)+1);
-images.push(`<img src="dices/dice${values[i]}.png" width ="150px" height="150px">`);
+images.push(`<img src="dice/dice${values[i]}.png" width ="150px" height="150px">`);
 }
 result.textContent = `dice : ${values.join(`, `)}`;
 imgs.innerHTML = images.join(``);
